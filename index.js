1ee0f9109dadb626cbfb3746c5e871dad38d4bc6
Eduardo <lalitro.es@gmail.com>
2019-02-26 09:27:19 -0700

    Lectura de un csv con fs

---
+++ index.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+fs.readFile('alumnos.csv', 'utf8', (err,data) => {
+    if(err) throw err;
+
+    var lineBk = data.split("\n");
+    var string = "";
+    for(var i=0; i < lineBk.length; i++){
+        var line = lineBk[i].split(",");
+        string += 'No. : '+ (i+1) + '\n' +
+                  'No. Control: '+ line[0] + '\n' +
+                  'Nombre: ' + line[1] + '\n' +
+                  'Calificación: ' + line[2] + '\n\n'
+    }
+    console.log(string);
+});
